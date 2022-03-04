f9d3a5411322af9f84182f086f2027f623d425e5
jraynolds <jaspirian@gmail.com>
2022-03-03 16:46:13 -0800

    Variable caching

--- src/App.vue
+++ src/App.vue
@@ -161,10 +161,10 @@
 							v-if="chosenEvent2.questions && chosenEvent2.questions.length"
 							v-show="event2Shown"
 						>
-							<RandomCard 
-								:colors="chosenEvent2.questions[0].colors"
-								:slotTitle="chosenEvent2.questions[0].name"
-								:small="true" 
+							<Card 
+								:cardObj="event2Answer1" 
+								:cardList="event2Answer1List" 
+								:slotTitle="chosenEvent2.questions[0]"
 							/>
 						</v-col>
 						<v-col 
@@ -173,10 +173,10 @@
 							v-if="chosenEvent2.questions && chosenEvent2.questions.length > 1"
 							v-show="event2Shown"
 						>
-							<RandomCard 
-								:colors="chosenEvent2.questions[1].colors"
-								:slotTitle="chosenEvent2.questions[1].name"
-								:small="true" 
+							<Card 
+								:cardObj="event2Answer2" 
+								:cardList="event2Answer2List" 
+								:slotTitle="chosenEvent2.questions[1]"
 							/>
 						</v-col>
 						<v-col 
@@ -185,10 +185,10 @@
 							v-if="chosenEvent2.questions && chosenEvent2.questions.length > 2"
 							v-show="event2Shown"
 						>
-							<RandomCard 
-								:colors="chosenEvent2.questions[2].colors"
-								:slotTitle="chosenEvent2.questions[2].name"
-								:small="true" 
+							<Card 
+								:cardObj="event2Answer3" 
+								:cardList="event2Answer3List" 
+								:slotTitle="chosenEvent2.questions[2]"
 							/>
 						</v-col>
 					</v-row>
@@ -219,6 +219,14 @@ export default {
 		chosenBond2: null,
 		chosenEvent1: null,
 		chosenEvent2: null,
+
+		event1Answer1: null,
+		event1Answer2: null,
+		event1Answer3: null,
+
+		event2Answer1: null,
+		event2Answer2: null,
+		event2Answer3: null,
 		
 		bond1Shown: false,
 		bond2Shown: false,
@@ -244,14 +252,85 @@ export default {
 			}
 			return card;
 		},
+		getRandomElementAndList(lists) {
+			let listLength = 0;
+			for (let list of lists) listLength += list.cards.length;
+
+			let index = Math.floor(Math.random() * listLength);
+			let i = 0;
+			let prevI = 0;
+			for (let list of lists) {
+				i += list.length;
+				if (index < i) {
+					return { item: list.cards[index - prevI], list };
+				}
+				prevI = i;
+			}
+			return { item: lists[0].cards[0], list: lists[0] };
+		},
+		cardLists() {
+			if (this.lists) return this.lists;
+			let lists = [];
+			if (this.colors) {
+				for (let color of this.colors) {
+					lists = lists.concat(this.cards.find(c => c.color == color));
+				}
+			}
+			return lists;
+		},
+		randomCard() {
+			let i = 0;
+			for (let list of this.cardLists) {
+				let iPrev = i;
+				i += list.cards.length;
+				if (this.randomNumber >= i) continue; 
+				let card = list.cards[this.randomNumber - iPrev]; 
+				return card;
+			}
+			return this.cardLists[0].cards[0];
+		},
+
 		roll() {
 			this.chosenPlace = this.getRandomElement(this.places);
 			this.chosenClass = this.getRandomElement(this.classes);
 			this.chosenBackground = this.getRandomElement(this.backgrounds);
 			this.chosenBond1 = this.getRandomElement(this.bonds);
 			this.chosenBond2 = this.getRandomElement(this.bonds);
+
 			this.chosenEvent1 = this.getRandomElement(this.events);
+			if (this.chosenEvent1.questions[0]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent1.questions[0].colors);
+				this.event1Answer1 = randomElementAndList.item;
+				this.event1Answer1List = randomElementAndList.list;
+			} 
+			if (this.chosenEvent1.questions[1]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent1.questions[1].colors);
+				this.event1Answer2 = randomElementAndList.item;
+				this.event1Answer2List = randomElementAndList.list;
+			} 
+			if (this.chosenEvent1.questions[2]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent1.questions[2].colors);
+				this.event1Answer3 = randomElementAndList.item;
+				this.event1Answer3List = randomElementAndList.list;
+			} 
+
 			this.chosenEvent2 = this.getRandomElement(this.events);
+			if (this.chosenEvent2.questions[0]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent2.questions[0].colors);
+				this.event2Answer1 = randomElementAndList.item;
+				this.event2Answer1List = randomElementAndList.list;
+			} 
+			if (this.chosenEvent2.questions[1]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent2.questions[1].colors);
+				this.event2Answer2 = randomElementAndList.item;
+				this.event2Answer2List = randomElementAndList.list;
+			} 
+			if (this.chosenEvent2.questions[2]) {
+				let randomElementAndList = this.getRandomElementAndList(this.chosenEvent2.questions[2].colors);
+				this.event2Answer3 = randomElementAndList.item;
+				this.event2Answer3List = randomElementAndList.list;
+			} 
+
 			this.event1Shown = false;
 			this.event2Shown = false;
 			this.bond1Shown = false;
